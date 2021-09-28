b18e2b3ea71a3a63aa531c91bb9d4465381a3364
Adityachess <adityadone9398@gmail.com>
2021-09-28 15:43:55 +0530

    ADD:Writing a program to check whether number entered by user is perfect square or not

--- LogicalProgramms.java
+++ LogicalProgramms.java
@@ -1,18 +1,25 @@
 package com.bridgelabz.logicalp;
 
+import java.util.Scanner;
+
 public class LogicalProgramms {
 
 	public static void main(String[] args) {
 
-		int a = 0, b = 1;
-		int temp;
-
-		for (int i = 1; i <= 10; i++) {
-			temp = a + b;
-			System.out.println(temp);
-			a = b;
-			b = temp;
+		int number, sum = 0;
+		System.out.println("enter the number:");
+		
+		Scanner sc = new Scanner(System.in);
+		number = sc.nextInt();
+		for (int i = 1; i < number; i++) {
+			if (number % i == 0) {
+				sum = sum + i;
+			}
 		}
+		if (number == sum)
+			System.out.println("Number is a perfect Sqarenumber");
+		else
+			System.out.println("Number is not a perfect Squarenumber");
 
 	}
 
